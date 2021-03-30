072ed53aff0bfadbf088957e48d57fc01267ae57
D7D7 <d7d7.learn@gmail.com>
2021-03-30 20:40:23 +0300

    playRound function added

--- scripts/script.js
+++ scripts/script.js
@@ -10,13 +10,13 @@ function computerPlay() {
 
     // COMPARE COMPUTER CHOICE => RETURN RESULT AS STRING
     if (computerNumber === 1) {
-        computerChoice = 'Rock';
+        computerChoice = 'rock';
         return computerChoice;
     } else if (computerNumber === 2) {
-        computerChoice = 'Paper';
+        computerChoice = 'paper';
         return computerChoice;
     } else {
-        computerChoice = 'Scissors';
+        computerChoice = 'scissors';
         return computerChoice;
     };
     
@@ -25,5 +25,24 @@ function computerPlay() {
 // COMPARE USERINPUT WITH COMPUTER CHOICE
 
 // function playRound(userInput, computerChoice) {
-//     if 
+
+    
+//     if (userInput != 'rock' || userInput != 'paper' || userInput != 'scissors') {
+//         console.log('Please Enter a valid choice and try again!');
+//     } else if (userInput === computerChoice) {
+//         return console.log('Tie!');
+//     } else if (userInput === 'rock' %% computerChoice === 'scissors') {
+//         return console.log('You Win!');
+//     } else if (userInput === 'scissors' && computerChoice === 'paper') {
+//         return console.log('You Win!');
+//     } else if (userInput === 'paper' && computerChoice === 'rock') {
+//         return console.log('You win!');
+//     } else {
+//         return console.log('You lose!');
+//     }
+
+    
+
 // }
+
+// playRound();
